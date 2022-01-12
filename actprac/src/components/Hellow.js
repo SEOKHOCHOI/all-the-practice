d6595d19d536475b883087e7ddb51de8281faf53
SEOKHOCHOI <talesost@naver.com>
2022-01-12 10:52:57 +0900

    props

--- actprac/src/components/Hellow.js
+++ actprac/src/components/Hellow.js
@@ -1,10 +1,9 @@
 import { useState } from "react";
-export default function Hello(props) {
-  console.log(props);
+import UserName from "./UserName";
+export default function Hello({ age }) {
   // let name = "Mike";
   const [name, setName] = useState('Mike');
-  const [age, setAge] = useState(props.age);
-
+  const msg = age > 19 ? "성인 입니다." : "미성년자 입니다.";
   // function changeName() {
   //   // const newName = name === "Mike" ? "Jane" : "Mike";
   //   // document.getElementById("name").innerText = name;
@@ -14,10 +13,10 @@ export default function Hello(props) {
   return (
     <div>
       <h1>state</h1>
-      <h2 id="name">{name}({age}세)</h2>
+      <UserName name={name} />
+      <h2 id="name">{name}({age}세) : {msg}</h2>
       <button onClick={() => {
         setName(name === "Mike" ? "Jane" : "Mike");
-        setAge(age + 1);
       }}>Change</button>
     </div>
   );
