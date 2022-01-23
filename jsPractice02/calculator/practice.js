1a39bae1d969be8095bad68eccbc519c2d4690fa
SEOKHOCHOI <talesost@naver.com>
2022-01-23 18:56:24 +0900

    toggle function으로 간단히!

--- jsPractice02/calculator/practice.js
+++ jsPractice02/calculator/practice.js
@@ -1,12 +1,7 @@
 const $title = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  const clickedClass = "clicked";
-  if ($title.classList.contains(clickedClass)) {
-    $title.classList.remove(clickedClass);
-  } else {
-    $title.classList.add(clickedClass);
-  }
+  $title.classList.toggle("clicked")
 }
 
 $title.addEventListener("click", handleTitleClick);
