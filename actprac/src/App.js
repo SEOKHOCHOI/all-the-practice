943635e4fdb8a56db8d9ba45bef852cd0aa8c4ea
SEOKHOCHOI <talesost@naver.com>
2022-02-20 22:13:25 +0900

    삭제

--- actprac/src/App.js
+++ actprac/src/App.js
@@ -1,35 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
+import Data from './Data';
+
 
 
 function App() {
+  const [book, setBook] = useState(Data.list[0]);
 
-  // 변수
-  let hi = '안뇽';
 
-  // 함수
-  function 함수이름() { return('리턴해줄거'); }
-
-  // img
-  // <img src=".jpg"> 잘안씀, <img src={logo}/>이런식으로 원하는위치에 넣어주셈
-
-  // style
   
-  
-  
-  let st = {color:'blue'}
- 
-
-
-  // state
-  const [이름0, 이름을변경해줄함수] = useState('최석호');
-  const [이름1, 이름변경] = useState(['최석호', '박우현']);
-  // {이름}, {이름[0]}, {이름[1]}
   return (
     <div className="App">
-      <h1 style={st}>{hi}</h1>
-      <h2 >{함수이름}</h2>
-      <h2>{ 이름1 }</h2>
+      <h1>{book.proTitle}</h1>
     </div>
   );
 }
---
+++ actprac/src/Data.js
@@ -0,0 +1,124 @@
+export default {
+  "list": [
+    {
+      "proCategory": "소설",
+      "proContents": "SF",
+      "proCountry": "KOR",
+      "proDate": "2022-01-26T08:27:39.929Z",
+      "proImage": "../images/Waterpuppy.jpg",
+      "proNum": 0,
+      "proPrice": 20000,
+      "proPublisher": "한빛",
+      "orderAmount": 1,
+      "proSales": 20000,
+      "proTitle": "책제목",
+      "proWriter": "톨킨"
+    },
+    {
+      "proCategory": "소설",
+      "proContents": "SF졸잼",
+      "proCountry": "KOR",
+      "proDate": "2022-01-26T08:27:39.929Z",
+      "proImage": "../images/Waterpuppy.jpg",
+      "proNum": 1,
+      "proPrice": 2000, // 가격
+      "proPublisher": "한빛",
+      "orderAmount": 1, // 주문 총계
+      "proSales": 2000, //판매된 권수
+      "proTitle": "책제목",
+      "proWriter": "톨킨"
+    },
+    {
+      "proCategory": "소설",
+      "proContents": "SF",
+      "proCountry": "KOR",
+      "proDate": "2022-01-26T08:27:39.929Z",
+      "proImage": "../images/Waterpuppy.jpg",
+      "proNum": 2,
+      "proPrice": 20000,
+      "proPublisher": "한빛",
+      "orderAmount": 1,
+      "proSales": 20000,
+      "proTitle": "책제목",
+      "proWriter": "톨킨"
+    },
+    {
+      "proCategory": "소설",
+      "proContents": "SF",
+      "proCountry": "KOR",
+      "proDate": "2022-01-26T08:27:39.929Z",
+      "proImage": "../images/Waterpuppy.jpg",
+      "proNum": 3,
+      "proPrice": 20000,
+      "proPublisher": "한빛",
+      "orderAmount": 1,
+      "proSales": 20000,
+      "proTitle": "책제목",
+      "proWriter": "톨킨"
+    },
+    {
+      "proCategory": "소설",
+      "proContents": "SF",
+      "proCountry": "KOR",
+      "proDate": "2022-01-26T08:27:39.929Z",
+      "proImage": "../images/Waterpuppy.jpg",
+      "proNum": 4,
+      "proPrice": 20000,
+      "proPublisher": "한빛",
+      "orderAmount": 1,
+      "proSales": 20000,
+      "proTitle": "책제목",
+      "proWriter": "톨킨"
+    },
+    {
+      "proCategory": "소설",
+      "proContents": "SF",
+      "proCountry": "KOR",
+      "proDate": "2022-01-26T08:27:39.929Z",
+      "proImage": "../images/Waterpuppy.jpg",
+      "proNum": 5,
+      "proPrice": 20000,
+      "proPublisher": "한빛",
+      "orderAmount": 1,
+      "proSales": 20000,
+      "proTitle": "책제목",
+      "proWriter": "톨킨"
+    },
+    {
+      "proCategory": "소설",
+      "proContents": "SF",
+      "proCountry": "KOR",
+      "proDate": "2022-01-26T08:27:39.929Z",
+      "proImage": "../images/Waterpuppy.jpg",
+      "proNum": 6,
+      "proPrice": 20000,
+      "proPublisher": "한빛",
+      "orderAmount": 1,
+      "proSales": 20000,
+      "proTitle": "책제목",
+      "proWriter": "톨킨"
+    },
+    {
+      "proCategory": "소설",
+      "proContents": "SF",
+      "proCountry": "KOR",
+      "proDate": "2022-01-26T08:27:39.929Z",
+      "proImage": "../images/Waterpuppy.jpg",
+      "proNum": 7,
+      "proPrice": 20000,
+      "proPublisher": "한빛",
+      "orderAmount": 1,
+      "proSales": 20000,
+      "proTitle": "책제목",
+      "proWriter": "톨킨"
+    },
+  ],
+  "pagingDto": {
+    "nowPage": 10,
+    "totalPage": 10,
+    "tupleCount": 100,
+    "hasNext" : false,
+    "blockSize" : 5,
+    "pageSize" : 50
+  }
+};
