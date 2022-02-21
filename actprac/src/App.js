6a6b4c26c3aabc6fe5154561ba40db4e6712ec7f
SEOKHOCHOI <talesost@naver.com>
2022-02-21 20:54:36 +0900

    - 장바구니 기능구현 (연습) 시작

--- actprac/src/App.js
+++ actprac/src/App.js
@@ -5,13 +5,19 @@ import Data from './Data';
 
 
 function App() {
-  const [book, setBook] = useState(Data.list[0]);
+
 
 
   
   return (
     <div className="App">
-      <h1>{book.proTitle}</h1>
+      <h1>Item</h1>
+      <div>
+        <h3>Item1</h3>
+        <h4>1억</h4>
+        <img src="https://search.pstatic.net/common/?src=http%3A%2F%2Fshopping.phinf.naver.net%2Fmain_2856167%2F28561672114.20210824233451.jpg&type=sc960_832" />
+        <button>장바구니</button>
+      </div>
     </div>
   );
 }
