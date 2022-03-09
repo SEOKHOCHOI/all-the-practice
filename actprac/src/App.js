1bf4b207b4ba92d142d65cc0f93d5518d3afa472
SEOKHOCHOI <talesost@naver.com>
2022-03-09 20:27:02 +0900

    - children 복습

--- actprac/src/App.js
+++ actprac/src/App.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Styles from "./App.module.css";
+import Layout from "./components/Layout";
+import Layout2 from "./components/Layout2";
 
 function App() {
-  return <div className={Styles.App}>연습</div>;
+  const [num, setNum] = useState(1);
+  const [item] = useState([1, 2, 3, 5, 6, 7, 8, 9]);
+
+  return (
+    <div>
+      <Layout className={Styles.App}>
+        {item.map((i, index) => {
+          return (
+            <div key={index}>
+              {num} * {i} = {num * i}
+            </div>
+          );
+        })}
+        <button onClick={() => setNum((prev) => prev + 1)}>+ 1</button>
+        <button onClick={() => setNum((prev) => (prev > 1 ? prev - 1 : prev))}>
+          - 1
+        </button>
+        <button onClick={() => setNum((prev) => prev * 2)}>* 2</button>
+      </Layout>
+      <Layout2>
+        <div>children 사용법</div>
+      </Layout2>
+    </div>
+  );
 }
 
 export default App;
